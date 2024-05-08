cf08d07e7cf44ba37b3e47a6898e3259e8a6dc2f
Pratik Rathod <pratik.rathod@elixirtechne.com>
2024-05-08 18:19:14 +0530

    fix - permission.py:role_permission

--- book_management/core/permission.py
+++ book_management/core/permission.py
@@ -2,22 +2,19 @@ from apps.users import crud
 from fastapi import HTTPException
 from functools import wraps, update_wrapper
 from fastapi import status
-from book_management.core import constant
+
 
 def role_permissions(roles: list | None | str):
-    def decorator_auth(func):
+    def decorator_perms(func):
         @wraps(func)
-        def wrapper_auth(*args, **kwargs):
-            print(args)
-            print(kwargs.get('user_id'))
-            
-            # user = crud.get_user_by_userid(kwargs['user_id'])
+        def wrapper_perms(*args, **kwargs):
+            user = crud.get_user_by_userid(args[0], args[1])
 
-            # if user.user_type not in roles:
-            #     raise HTTPException(
-            #         status_code=status.HTTP_403_FORBIDDEN,
-            #         detail={"error": "You are not allowed to perform this action"}
-            #     )
+            if user.user_type not in roles:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail={"error": "You are not allowed to perform this action"}
+                )
             return func(*args, **kwargs)
-        return update_wrapper(wrapper_auth, func)
-    return decorator_auth
+        return update_wrapper(wrapper_perms, func)
+    return decorator_perms
